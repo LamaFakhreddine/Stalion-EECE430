fbba04528aa9b094967143b956934593356deb80
Lama Fakhreddine <77051181+LamaFakhreddine@users.noreply.github.com>
2022-04-05 13:47:15 +0300

    initial db design

--- stallion_app/stallion_website/admin.py
+++ stallion_app/stallion_website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import * 
 # Register your models here.
+
+admin.site.register(Member)
+admin.site.register(Event)
+admin.site.register(Tickets)
+admin.site.register(EventTickets)
+admin.site.register(Program)
+admin.site.register(Date)
