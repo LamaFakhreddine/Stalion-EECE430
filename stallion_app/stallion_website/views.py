e9fcf52c26ff24fd4a384e9cd159c84484501a7b
Lama Fakhreddine <77051181+LamaFakhreddine@users.noreply.github.com>
2022-04-09 21:32:46 +0300

    fixed search bar

--- stallion_app/stallion_website/views.py
+++ stallion_app/stallion_website/views.py
@@ -1,30 +1,48 @@
 from datetime import datetime
 from multiprocessing import Event
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import * 
 import calendar
 # Create your views here.
 
 def home(request):
-    next_event = Event.objects.filter().order_by('datetime')[0]
-    today = datetime.today()
-    print(today)
+    now = datetime.now()
+    next_event = Event.objects.filter(datetime__gt = now).order_by('datetime')[0]
     context = {
         "next_event": next_event
     }
     return render(request, 'stallion_website/index.html', context=context)
 
 def events(request):
-    events = Event.objects.filter().order_by('datetime')
+    # get all events whose date has not passed yet 
+    today = datetime.today()
+    events = Event.objects.filter(datetime__gt = today).order_by('datetime')
     next_event = events[0]
-    context = {
-        "events": events,
-        "next_event": next_event
-    }
-    print(events)
-    return render(request, 'stallion_website/events.html', context=context)
+
+    if request.method == 'GET':
+        context = {
+            "events": events,
+            "next_event": next_event
+        }
+        print(events)
+        return render(request, 'stallion_website/events.html', context=context)
     
+    if request.method == 'POST':
+        filter = request.POST.get("filter")
+        # if there is no filter, redirect to page
+        # else render the page with the new filtered events 
+        if filter is None or filter == "":
+            return redirect("events")    
+        events = Event.objects.filter(name__contains=filter).order_by('datetime')
+        context = {
+            "events": events,
+            "next_event": next_event
+        }
+        return render(request, 'stallion_website/events.html', context=context)
+    else:
+        render("Bad request!")  
+
 def about(request):
     return HttpResponse("ABOUT US")
 
